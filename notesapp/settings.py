598929acd13111f7f8a79f1cc75d7f690601c912
Arbz-N <arbaznaeem555@gmail.com>
2026-01-21 06:24:50 +0500

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),      # ← None
-        'USER': os.getenv("DB_USER"),      # ← None
-        'PASSWORD': os.getenv("DB_PASSWORD"),  # ← None
-        'HOST': os.getenv("DB_HOST"),      # ← None
-        'PORT': os.getenv("DB_PORT"),      # ← None
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
